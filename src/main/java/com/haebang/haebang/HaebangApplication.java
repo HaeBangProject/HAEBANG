a9b868b8e8de81dab2645aba47575843055bb76d
JYKANG <xadamantiumx@naver.com>
2023-03-30 15:23:41 +0900

    환경설정

---
+++ src/main/java/com/haebang/haebang/HaebangApplication.java
@@ -0,0 +1,13 @@
+package com.haebang.haebang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaebangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaebangApplication.class, args);
+	}
+
+}
