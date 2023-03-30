02b727f1be68d9ed250ee034f4d171565f5fe84b
JYKANG <xadamantiumx@naver.com>
2023-03-30 16:23:22 +0900

    0330-1623

--- src/main/java/com/haebang/haebang/controller/HomeController.java
+++ src/main/java/com/haebang/haebang/controller/HomeController.java
@@ -1,5 +1,16 @@
 package com.haebang.haebang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
+    @GetMapping("hello")
+    public String homeController(){
+        return "map";
+    }
 }
 //
--- src/main/java/com/haebang/haebang/controller/MapController.java
+++ src/main/java/com/haebang/haebang/controller/MapController.java
@@ -1,5 +1,18 @@
 package com.haebang.haebang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
 public class MapController {
+
+    @RequestMapping("/map")
+    public String map(Model model){
+        model.addAttribute("test","test");
+        return "map";
+    }
+
+
 }
-//
