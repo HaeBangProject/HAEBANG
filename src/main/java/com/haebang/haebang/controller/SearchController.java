5f91fdbd4bef560423207f959bb649a6f64dd626
JYKANG <xadamantiumx@naver.com>
2023-04-19 17:10:09 +0900

    카카오 맵 마커 위에 정보 띄우기 및 json 데이터 파싱

--- src/main/java/com/haebang/haebang/controller/SearchController.java
+++ src/main/java/com/haebang/haebang/controller/SearchController.java
@@ -1,5 +1,10 @@
 package com.haebang.haebang.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class SearchController {
+
 }
-//
