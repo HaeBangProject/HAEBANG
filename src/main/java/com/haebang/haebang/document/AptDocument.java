fb1e1e4cf2be7ba2432b648633f3cfe42c25f6ef
JYKANG <xadamantiumx@naver.com>
2023-08-13 00:09:43 +0900

    브랜치 수정

--- src/main/java/com/haebang/haebang/document/AptDocument.java
+++ src/main/java/com/haebang/haebang/document/AptDocument.java
@@ -14,7 +14,7 @@ import javax.persistence.Id;
 public class AptDocument {
     @Id
     private Long id;
-    @Field(type = FieldType.Text)
+    @Field(type = FieldType.Text) // analyzer = "my_analyzer"
     String roadAddress;// 서울 00구 이태원로27가길 49
     @Field(type = FieldType.Text)
     String dp;// 한 건물당 하나의 도로명주소가짐 (아파트/건물이름)
--- src/main/java/com/haebang/haebang/dto/ChatMessageDTO.java
+++ src/main/java/com/haebang/haebang/dto/ChatMessageDTO.java
@@ -7,12 +7,8 @@ import lombok.Setter;
 @Setter
 public class ChatMessageDTO {
 
-    // 메시지 타입 : 입장, 채팅
-    public enum MessageType {
-        ENTER, TALK
-    }
-    private MessageType type; // 메시지 타입
+
     private String roomId; // 방번호
-    private String sender; // 메시지 보낸사람
+    private String writer; // 메시지 보낸사람
     private String message; // 메시지
 }
