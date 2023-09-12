e44184171623dfd91bc871e4c69f958043ddf4b9
jisoo <ske050588@gmail.com>
2023-09-12 12:45:07 +0900

    패스워드 특수문자,숫자,영문 조합 추가

--- src/main/java/com/haebang/haebang/dto/JoinDto.java
+++ src/main/java/com/haebang/haebang/dto/JoinDto.java
@@ -4,6 +4,7 @@ import lombok.*;
 
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Builder
 @AllArgsConstructor
@@ -13,7 +14,9 @@ public class JoinDto {
     @NotBlank(message = "유저이름은 필수 입력값입니다")
     private String username;
     @NotBlank(message = "비밀번호는 필수 입력값입니다")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
-    @NotBlank(message = "이메일은 필수 입력값입니다") @Email(message = "이메일 형식을 지켜주세요")
+    @NotBlank(message = "이메일은 필수 입력값입니다")
+    @Email(message = "이메일 형식을 지켜주세요")
     private String email;
 }
--- src/main/java/com/haebang/haebang/dto/LoginDto.java
+++ src/main/java/com/haebang/haebang/dto/LoginDto.java
@@ -3,6 +3,7 @@ package com.haebang.haebang.dto;
 import lombok.*;
 
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
 
 @Builder
 @AllArgsConstructor
@@ -12,5 +13,6 @@ public class LoginDto {
     @NotBlank(message = "유저이름은 필수 입력값입니다")
     private String username;
     @NotBlank(message = "비밀번호는 필수 입력값입니다")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z])(?=.*\\W)(?=\\S+$).{8,16}", message = "비밀번호는 8~16자 영문 대 소문자, 숫자, 특수문자를 사용하세요.")
     private String password;
 }
